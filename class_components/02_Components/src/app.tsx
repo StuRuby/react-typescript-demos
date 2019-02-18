1919d65b57d39e35f1571fb9fb0169f6bb93211f
StuRuby <937931249@qq.com>
2019-02-19 07:27:35 +0800

    Add components code

---
+++ class_components/02_Components/src/app.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { Header, About } from './components';
+
+export const App: React.StatelessComponent<{}> = () => (
+    <div className='container-fluid'>
+        <Header/>
+        <About />
+    </div>
+);
