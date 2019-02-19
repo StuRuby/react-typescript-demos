5850548aea3fb7f4949e7d56a69ba4581d65872b
StuRuby <937931249@qq.com>
2019-02-19 23:00:59 +0800

    Add display data demo

--- class_components/04_DisplayData/src/api/member/mockData.ts
+++ class_components/04_DisplayData/src/api/member/mockData.ts
@@ -4,11 +4,11 @@ export const members: MemberEntity[] = [
     {
         id: 1457912,
         login: 'shixy',
-        avatar_url: 'https://avatars.githubusercontent.com/u/1457912?v=3'
+        avatar_url: 'https://avatars0.githubusercontent.com/u/17522995?s=460&v=4'
     },
     {
         id: 1566577,
         login: 'yangdl',
-        avatar_url: 'https://avatars.githubusercontent.com/u/4374977?v=3'
+        avatar_url: 'https://avatars1.githubusercontent.com/u/39565455?s=460&v=4'
     }
 ];
