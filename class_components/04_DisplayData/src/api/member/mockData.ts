1dbc6255375343dfbe9794d1913e89e0c51f4753
StuRuby <937931249@qq.com>
2019-02-19 22:16:35 +0800

    Add display data table

---
+++ class_components/04_DisplayData/src/api/member/mockData.ts
@@ -0,0 +1,14 @@
+import { MemberEntity } from '../../model';
+
+export const members: MemberEntity[] = [
+    {
+        id: 1457912,
+        login: 'shixy',
+        avatar_url: 'https://avatars.githubusercontent.com/u/1457912?v=3'
+    },
+    {
+        id: 1566577,
+        login: 'yangdl',
+        avatar_url: 'https://avatars.githubusercontent.com/u/4374977?v=3'
+    }
+];
