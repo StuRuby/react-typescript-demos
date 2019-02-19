61e0559c995ce0428cf741462a6e5798b5090468
StuRuby <937931249@qq.com>
2019-02-20 07:26:53 +0800

    Copy code from 05_PresentationalComponent

---
+++ class_components/06_AjaxCall/src/components/members/memberRow.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { MemberEntity } from "../../model";
+
+interface Props {
+    member: MemberEntity;
+}
+
+export const MemberRow: React.StatelessComponent<Props> = ({ member }) => (
+    <tr>
+        <td>
+            <img src={member.avatar_url} className="avatar" />
+        </td>
+        <td>
+            <span>{member.id}</span>
+        </td>
+        <td>
+            <span>{member.login}</span>
+        </td>
+    </tr>
+);
