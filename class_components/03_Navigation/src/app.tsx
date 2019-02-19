095ca37824361a372304405b5b37262aa9e6951d
StuRuby <937931249@qq.com>
2019-02-19 15:15:26 +0800

    Init typescript react navigation webpack config

---
+++ class_components/03_Navigation/src/app.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Button from 'antd/lib/button';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className='App'>
+                <Button type='primary'>Button</Button>
+            </div>
+        );
+    }
+}
+
+export default App;
