dea6faef5058cbf7d17f6cfe86dad04286bd3d7a
StuRuby <937931249@qq.com>
2019-02-20 07:20:00 +0800

    Separate presentational code from source

--- class_components/05_PresentationalComponent/src/components/members/page.tsx
+++ class_components/05_PresentationalComponent/src/components/members/page.tsx
@@ -1,54 +1,40 @@
 import * as React from "react";
 import { MemberEntity } from "../../model";
 import { memberAPI } from "../../api/member";
+import { MemberHeader } from "./memberHeader";
+import { MemberRow } from "./memberRow";
 
 interface State {
-  members: MemberEntity[];
+    members: MemberEntity[];
 }
 
 interface Props {}
 
-const MemberHeader = () => (
-  <tr>
-    <th>Avatar</th>
-    <th>Id</th>
-    <th>Name</th>
-  </tr>
-);
-
-const MemberRow = (member: MemberEntity) => (
-  <tr key={member.id}>
-    <td>
-      <img src={member.avatar_url} className="avatar" />
-    </td>
-    <td>
-      <span>{member.id}</span>
-    </td>
-    <td>
-      <span>{member.login}</span>
-    </td>
-  </tr>
-);
-
 export class MembersPage extends React.Component<Props, State> {
-  constructor(props) {
-    super(props);
-    this.state = { members: [] };
-  }
+    constructor(props) {
+        super(props);
+        this.state = { members: [] };
+    }
 
-  componentDidMount() {
-    memberAPI.fetchMembers().then(members => this.setState({ members }));
-  }
+    componentDidMount() {
+        memberAPI.fetchMembers().then(members => this.setState({ members }));
+    }
 
-  render() {
-    return (
-      <div className="row">
-        <h2>Members Page</h2>
-        <table className="table">
-          <thead>{MemberHeader()}</thead>
-          <tbody>{this.state.members.map(MemberRow)}</tbody>
-        </table>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="row">
+                <h2>Members Page</h2>
+                <table className="table">
+                    <thead>
+                        <MemberHeader />
+                    </thead>
+                    <tbody>
+                        {this.state.members.map(member => (
+                            <MemberRow key={member.id} member={member} />
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
 }
