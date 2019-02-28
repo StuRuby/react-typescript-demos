2edfa06eee182bdc10abd410fbe54aa60db6f52b
StuRuby <937931249@qq.com>
2019-02-28 13:14:45 +0800

    Update router

--- class_components/09_Redux/src/router.tsx
+++ class_components/09_Redux/src/router.tsx
@@ -1,19 +1,23 @@
 import * as React from 'react';
-import { Route, Switch, HashRouter } from 'react-router-dom';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store';
 import { App } from './app';
 import { About, MembersPage, MemberPageContainer } from './components';
 
 export const AppRouter: React.StatelessComponent<{}> = () => (
-    <HashRouter>
-        <div className="container-fluid">
-            <Route path="/" component={App} />
-            <Switch>
-                <Route exact path="/" component={About} />
-                <Route path="/about" component={About} />
-                <Route path="/members" component={MembersPage} />
-                <Route path="/member" component={MemberPageContainer} />
-                <Route path="/member/:id" component={MemberPageContainer} />
-            </Switch>
-        </div>
-    </HashRouter>
+    <Provider store={store}>
+        <Router>
+            <div className="container-fluid">
+                <Route path="/" component={App} />
+                <Switch>
+                    <Route exact path="/" component={About} />
+                    <Route path="/about" component={About} />
+                    <Route path="/members" component={MembersPage} />
+                    <Route path="/member" component={MemberPageContainer} />
+                    <Route exact path="/member/:id" component={MemberPageContainer} />
+                </Switch>
+            </div>
+        </Router>
+    </Provider>
 );
