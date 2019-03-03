56a058346fa456ce302b206ef6dde977741c4baf
StuRuby <937931249@qq.com>
2019-03-03 19:42:37 +0800

    Add reducers

---
+++ class_components/11_TodoApp/src/reducers/todos.ts
@@ -0,0 +1,71 @@
+import { Action } from '../actions';
+import {
+    ADD_TODO,
+    DELETE_TODO,
+    EDIT_TODO,
+    COMPLETE_ALL_TODO,
+    COMPLETE_TODO,
+    CLEAR_COMPLETED,
+    ACTION_TYPES
+} from '../constants/ActionTypes';
+
+interface State {
+    id: number;
+    text: string;
+    completed: boolean;
+}
+
+const initialState = [
+    {
+        id: 0,
+        text: 'Use Redux with Typescript',
+        completed: false
+    }
+];
+
+export default function todos(state: State[] = initialState, action: Action) {
+    switch (action.type) {
+        case ADD_TODO:
+            return [
+                ...state,
+                {
+                    id:
+                        state.reduce(
+                            (maxId, todo) => Math.max(todo.id, maxId),
+                            -1
+                        ) + 1,
+                    completed: false,
+                    text: action.text
+                }
+            ];
+        case DELETE_TODO:
+            return state.filter(todo => todo.id !== action.id);
+        case EDIT_TODO:
+            return state.map(todo =>
+                todo.id === action.id
+                    ? {
+                          ...todo,
+                          text: action.text
+                      }
+                    : todo
+            );
+        case COMPLETE_TODO:
+            return state.map(todo =>
+                todo.id === action.id
+                    ? {
+                          ...todo,
+                          completed: !todo.completed
+                      }
+                    : todo
+            );
+        case COMPLETE_ALL_TODO:
+            return state.map(todo => ({
+                ...todo,
+                completed: !state.every(item => item.completed)
+            }));
+        case CLEAR_COMPLETED:
+            return state.filter(todo => todo.completed !== true);
+        default:
+            return state;
+    }
+}
