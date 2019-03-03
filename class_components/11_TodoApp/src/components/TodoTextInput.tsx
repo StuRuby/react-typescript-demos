3c60b894b2516a882ad17fa06f7c10e146d139bc
StuRuby <937931249@qq.com>
2019-03-03 20:58:47 +0800

    Add todoTextInput component

---
+++ class_components/11_TodoApp/src/components/TodoTextInput.tsx
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import * as classnames from 'classnames';
+
+interface Props {
+    text?: string;
+    editing?: boolean;
+    newTodo?: boolean;
+    onSave: (text: string) => void;
+    placeholder?: string;
+}
+
+interface State {
+    text: string;
+}
+
+export default class TodoTextInput extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            text: this.props.text || ''
+        };
+    }
+    private handleBlur = (e: any) => {
+        if (!this.props.newTodo) {
+            this.props.onSave(e.target.value);
+        }
+    };
+
+    private handleChange = (e: any) => {
+        this.setState({
+            text: e.target.value
+        });
+    };
+
+    private handleSubmit = (e: any) => {
+        const value = e.target.value.trim();
+        if (e.which === 13) {
+            this.props.onSave(value);
+            if (this.props.newTodo) {
+                this.setState({ text: '' });
+            }
+        }
+    };
+
+    render() {
+        const classname = this.props.newTodo ? `new-todo edit` : 'edit';
+        return (
+            <input
+                type="text"
+                className={classname}
+                placeholder={this.props.placeholder}
+                autoFocus={true}
+                value={this.props.text}
+                onBlur={this.handleBlur}
+                onChange={this.handleChange}
+                onKeyDown={this.handleSubmit}
+            />
+        );
+    }
+}
