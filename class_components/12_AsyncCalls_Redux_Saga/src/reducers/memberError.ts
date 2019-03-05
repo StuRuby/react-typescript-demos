4d9bfce8c896d6e4a28fd24ab6700de100e3370a
StuRuby <937931249@qq.com>
2019-03-05 08:28:16 +0800

    Update 12 demo tsconfig.json

--- class_components/12_AsyncCalls_Redux_Saga/src/reducers/memberError.ts
+++ class_components/12_AsyncCalls_Redux_Saga/src/reducers/memberError.ts
@@ -1,41 +1,53 @@
-import { FieldValidationResult, FormValidationResult } from 'lc-form-validation';
+import {
+    FieldValidationResult,
+    FormValidationResult
+} from 'lc-form-validation';
 import { actionTypes } from '../common/constants/actionTypes';
 import { MemberErrors } from '../model';
 import { MemberFieldChangePayload } from '../components/member/actions/memberFieldChange';
 
 const createEmptyMemberErrors = (): MemberErrors => ({
-  login: new FieldValidationResult(),
+    login: new FieldValidationResult()
 });
 
-export const memberErrorReducer = (state = createEmptyMemberErrors(), action) => {
-  switch (action.type) {
-    // case actionTypes.FETCH_MEMBERS_COMPLETED:
-    //   return handleFetchMembersCompleted(state, action.payload);
-    case actionTypes.UPDATE_MEMBER_FIELD:
-      return handleUpdateMemberField(state, action.payload);
-    case actionTypes.SAVE_MEMBER:
-      return handleSaveMember(state, action.payload);
-  }
+export const memberErrorReducer = (
+    state = createEmptyMemberErrors(),
+    action
+) => {
+    switch (action.type) {
+        // case actionTypes.FETCH_MEMBERS_COMPLETED:
+        //   return handleFetchMembersCompleted(state, action.payload);
+        case actionTypes.UPDATE_MEMBER_FIELD:
+            return handleUpdateMemberField(state, action.payload);
+        case actionTypes.SAVE_MEMBER:
+            return handleSaveMember(state, action.payload);
+    }
 
-  return state;
+    return state;
 };
 
 const handleFetchMembersCompleted = (state: MemberErrors, payload) => {
-  return createEmptyMemberErrors();
+    return createEmptyMemberErrors();
 };
 
-const handleUpdateMemberField = (state: MemberErrors, payload: MemberFieldChangePayload): MemberErrors => {
-  return {
-    ...state,
-    [payload.fieldValidationResult.key]: payload.fieldValidationResult,
-  };
+const handleUpdateMemberField = (
+    state: MemberErrors,
+    payload: MemberFieldChangePayload
+): MemberErrors => {
+    return {
+        ...state,
+        [payload.fieldValidationResult.key]: payload.fieldValidationResult
+    };
 };
 
-const handleSaveMember = (state: MemberErrors, payload: FormValidationResult): MemberErrors => {
-  const newMemberErrors = { ...state };
+const handleSaveMember = (
+    state: MemberErrors,
+    payload: FormValidationResult
+): MemberErrors => {
+    const newMemberErrors = { ...state };
 
-  return payload.fieldErrors.reduce((memberErrors, fieldValidationResult) => {
-    memberErrors[fieldValidationResult.key] = fieldValidationResult;
-    return memberErrors;
-  }, newMemberErrors);
+    return payload.fieldErrors.reduce((memberErrors, fieldValidationResult) => {
+        memberErrors[fieldValidationResult.key] = fieldValidationResult;
+        return memberErrors;
+    }, newMemberErrors);
 };
